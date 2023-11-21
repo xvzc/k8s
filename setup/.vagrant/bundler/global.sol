0babcd5ed07b9932ed9d730ef53e9a251f34bc73
xvzc <dev.kwanghoo@gmail.com>
2023-11-21 17:58:15 +0900

    Init

---
+++ setup/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"e75239a733d7fc37f57f89140039fb29bd89f674dec6c773f7531d188b1f2cb1","vagrant_version":"2.4.0"}
